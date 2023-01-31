9d054f79550123c0bc657e0e9ad8659c85cd5e0c
Rashed Pervez <rpervez02@gmail.com>
2023-01-31 11:52:47 +0600

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Apple iMac on 31/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
